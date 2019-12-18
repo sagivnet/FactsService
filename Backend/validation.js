6e082b65d203ec2502925174d53ab78ed16466d1
sagivnet <sagivnet@gmail.com>
2019-12-18 02:27:02 +0200

    design

--- Backend/validation.js
+++ Backend/validation.js
@@ -5,7 +5,7 @@ const Joi = require('@hapi/joi')
 const registerValidation = (data) => {
     const schema = {
         name: Joi.string()
-            .min(3)
+            .min(1)
             .required(),
         email: Joi.string()
             .min(6)
